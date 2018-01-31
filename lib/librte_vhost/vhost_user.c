44541f6a366160feafb25ce6034a7207113fe68e
Tomasz Kulasek <tomaszx.kulasek@intel.com>
2018-01-31 18:44:50 +0100

    vhost: destroy vhost device before updating public vring data
    For now DPDK assumes that callfd, kickfd and last_idx are being set just
    once during vring initialization and device cannot be running while DPDK
    receives SET_VRING_KICK, SET_VRING_CALL and SET_VRING_BASE messages.
    However, that assumption is wrong. For Vhost SCSI messages might arrive
    at any point of time, possibly multiple times, one after another.
    
    QEMU issues SET_VRING_CALL once during device initialization, then again
    during device start. The second message will close previous callfd,
    which is still being used by the user-implementation of vhost device.
    This results in writing to invalid (closed) callfd.
    
    This patch destroys vhost device before setting callfd, kickfd and last
    vring indices. It will be recreated right after (with updated vring
    data).
    
    Change-Id: I51261d6f72943204d0782b16f0ed8f707ccfbc57
    Signed-off-by: Dariusz Stojaczyk <dariuszx.stojaczyk@intel.com>
    Signed-off-by: Tomasz Kulasek <tomaszx.kulasek@intel.com>

--- lib/librte_vhost/vhost_user.c
+++ lib/librte_vhost/vhost_user.c
@@ -516,6 +516,12 @@ static int
 vhost_user_set_vring_base(struct virtio_net *dev,
 			  VhostUserMsg *msg)
 {
+	/* Remove from the data plane. */
+	if (dev->flags & VIRTIO_DEV_RUNNING) {
+		dev->flags &= ~VIRTIO_DEV_RUNNING;
+		dev->notify_ops->destroy_device(dev->vid);
+	}
+
 	dev->virtqueue[msg->payload.state.index]->last_used_idx  =
 			msg->payload.state.num;
 	dev->virtqueue[msg->payload.state.index]->last_avail_idx =
@@ -819,6 +825,12 @@ vhost_user_set_vring_call(struct virtio_net *dev, struct VhostUserMsg *pmsg)
 	struct vhost_vring_file file;
 	struct vhost_virtqueue *vq;
 
+	/* Remove from the data plane. */
+	if (dev->flags & VIRTIO_DEV_RUNNING) {
+		dev->flags &= ~VIRTIO_DEV_RUNNING;
+		dev->notify_ops->destroy_device(dev->vid);
+	}
+
 	file.index = pmsg->payload.u64 & VHOST_USER_VRING_IDX_MASK;
 	if (pmsg->payload.u64 & VHOST_USER_VRING_NOFD_MASK)
 		file.fd = VIRTIO_INVALID_EVENTFD;
@@ -840,6 +852,12 @@ vhost_user_set_vring_kick(struct virtio_net **pdev, struct VhostUserMsg *pmsg)
 	struct vhost_vring_file file;
 	struct vhost_virtqueue *vq;
 	struct virtio_net *dev = *pdev;
+
+	/* Remove from the data plane. */
+	if (dev->flags & VIRTIO_DEV_RUNNING) {
+		dev->flags &= ~VIRTIO_DEV_RUNNING;
+		dev->notify_ops->destroy_device(dev->vid);
+	}
 
 	file.index = pmsg->payload.u64 & VHOST_USER_VRING_IDX_MASK;
 	if (pmsg->payload.u64 & VHOST_USER_VRING_NOFD_MASK)
