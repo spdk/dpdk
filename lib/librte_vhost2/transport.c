5fe1cd530e750107b345ca44f9e4dbcc4f863aa0
Dariusz Stojaczyk <dariuszx.stojaczyk@intel.com>
2018-06-03 10:22:31 +0200

    vhost2: add initial implementation
    This is the initial public API. Although it incorporates
    some of the Vhost-user specifics, the API is meant to be
    as generic is possible. It will hopefully allow completely
    new vhost transports/protocols to reuse the same interface
    in future. Subsequent patches in this series add extra
    functionalities and extend this interface a bit, although
    its general structure remains the same.
    
    This patch also comes with a `transport` layer that's meant
    to propagate all user requests to the proper transport
    specific handlers. This layer makes the rte_vhost2.h API
    fully implemented, but no transports are available yet,
    hence there's no point in using this whole lib just yet.
    vhost-user socket transport is added in subsequent patches.
    
    Meson build is not supported yet.
    
    Change-Id: If33a657fca3d8ac0324016c50a379c64502ba17e
    Signed-off-by: Dariusz Stojaczyk <dariuszx.stojaczyk@intel.com>

---
+++ lib/librte_vhost2/transport.c
@@ -0,0 +1,74 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) Intel Corporation
+ */
+
+#include <string.h>
+#include <errno.h>
+#include <pthread.h>
+#include <sys/queue.h>
+
+#include "transport.h"
+#include "rte_vhost2.h"
+
+static pthread_mutex_t g_vhost_mutex = PTHREAD_MUTEX_INITIALIZER;
+TAILQ_HEAD(, vhost_transport_ops) g_vhost_transports =
+		TAILQ_HEAD_INITIALIZER(g_vhost_transports);
+
+int
+rte_vhost2_tgt_register(const char *trtype, const char *trid,
+		uint64_t trflags, void *trctx,
+		const struct rte_vhost2_tgt_ops *tgt_ops,
+		uint64_t features)
+{
+	struct vhost_transport_ops *tr;
+	int rc = -EINVAL;
+
+	pthread_mutex_lock(&g_vhost_mutex);
+	TAILQ_FOREACH(tr, &g_vhost_transports, tailq) {
+		if (strcmp(trtype, tr->type) == 0) {
+			rc = tr->tgt_register(trid, trflags, trctx, tgt_ops,
+				features);
+			break;
+		}
+	}
+	pthread_mutex_unlock(&g_vhost_mutex);
+	return rc;
+}
+
+int
+rte_vhost2_tgt_unregister(const char *trtype, const char *trid,
+		void (*cb_fn)(void *arg), void *cb_ctx)
+{
+	struct vhost_transport_ops *tr;
+	int rc = -EINVAL;
+
+	pthread_mutex_lock(&g_vhost_mutex);
+	TAILQ_FOREACH(tr, &g_vhost_transports, tailq) {
+		if (strcmp(trtype, tr->type) == 0) {
+			rc = tr->tgt_unregister(trid, cb_fn, cb_ctx);
+			break;
+		}
+	}
+	pthread_mutex_unlock(&g_vhost_mutex);
+	return rc;
+}
+
+void
+rte_vhost2_dev_op_complete(struct rte_vhost2_dev *vdev, int rc)
+{
+	vdev->transport->dev_op_cpl(vdev, rc);
+}
+
+void
+rte_vhost2_dev_call(struct rte_vhost2_dev *vdev, struct rte_vhost2_vq *vq)
+{
+	vdev->transport->dev_call(vdev, vq);
+}
+
+void
+vhost_transport_register(struct vhost_transport_ops *transport)
+{
+	pthread_mutex_lock(&g_vhost_mutex);
+	TAILQ_INSERT_TAIL(&g_vhost_transports, transport, tailq);
+	pthread_mutex_unlock(&g_vhost_mutex);
+}
