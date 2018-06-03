51940d94ce6b33cbab33e353561ae30b5916611c
Dariusz Stojaczyk <dariuszx.stojaczyk@intel.com>
2018-06-03 10:22:31 +0200

    vhost2: add initial implementation
    Change-Id: If33a657fca3d8ac0324016c50a379c64502ba17e
    Signed-off-by: Dariusz Stojaczyk <dariuszx.stojaczyk@intel.com>

---
+++ lib/librte_vhost2/transport.c
@@ -0,0 +1,84 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) Intel Corporation
+ */
+
+#include <linux/vhost.h>
+#include <linux/virtio_net.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#ifdef RTE_LIBRTE_VHOST_NUMA
+#include <numaif.h>
+#endif
+
+#include <rte_errno.h>
+#include <rte_ethdev.h>
+#include <rte_log.h>
+#include <rte_string_fns.h>
+#include <rte_memory.h>
+#include <rte_malloc.h>
+
+#include "transport.h"
+#include "rte_vhost2.h"
+
+static pthread_mutex_t g_vhost_mutex = PTHREAD_MUTEX_INITIALIZER;
+TAILQ_HEAD(, vhost_transport_ops) g_vhost_transports =
+		TAILQ_HEAD_INITIALIZER(g_vhost_transports);
+
+int
+rte_vhost2_tgt_register(const char *trtype, const char *trid,
+			uint64_t trflags, void *trctx,
+			const struct rte_vhost2_tgt_ops *tgt_ops,
+			uint64_t features)
+{
+	struct vhost_transport_ops *tr;
+	int rc = -EINVAL;
+
+	pthread_mutex_lock(&g_vhost_mutex);
+	TAILQ_FOREACH(tr, &g_vhost_transports, tailq) {
+		if (strcmp(trtype, tr->type) == 0) {
+			rc = tr->tgt_register(trid, trflags, trctx, tgt_ops, features);
+			break;
+		}
+	}
+	pthread_mutex_unlock(&g_vhost_mutex);
+	return rc;
+}
+
+int
+rte_vhost2_tgt_unregister(const char *trtype, const char *trid,
+			       void (*cb_fn)(void *arg), void *cb_ctx)
+{
+	struct vhost_transport_ops *tr;
+	int rc = -EINVAL;
+
+	pthread_mutex_lock(&g_vhost_mutex);
+	TAILQ_FOREACH(tr, &g_vhost_transports, tailq) {
+		if (strcmp(trtype, tr->type) == 0) {
+			rc = tr->tgt_unregister(trid, cb_fn, cb_ctx);
+			break;
+		}
+	}
+	pthread_mutex_unlock(&g_vhost_mutex);
+	return rc;
+}
+
+void
+rte_vhost2_dev_op_complete(struct rte_vhost2_dev *vdev, int rc)
+{
+	vdev->transport->dev_op_cpl(vdev, rc);
+}
+
+void
+rte_vhost2_dev_call(struct rte_vhost2_dev *vdev, struct rte_vhost2_vq *vq)
+{
+	vdev->transport->dev_call(vdev, vq);
+}
+
+void
+vhost_transport_register(struct vhost_transport_ops *transport)
+{
+	pthread_mutex_lock(&g_vhost_mutex);
+	TAILQ_INSERT_TAIL(&g_vhost_transports, transport, tailq);
+	pthread_mutex_unlock(&g_vhost_mutex);
+}
---
+++ lib/librte_vhost2/transport.h
@@ -0,0 +1,32 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) Intel Corporation
+ */
+
+#ifndef _RTE_VHOST2_TRANSPORT_H_
+#define _RTE_VHOST2_TRANSPORT_H_
+
+#include "rte_vhost2.h"
+
+struct vhost_transport_ops {
+	char type[32];
+	int (*tgt_register)(const char *trid, uint64_t trflags, void *trctx,
+			const struct rte_vhost2_tgt_ops *tgt_ops,
+			uint64_t features);
+	int (*tgt_unregister)(const char *trid,
+			void (*cb_fn)(void *arg), void *cb_arg);
+	void (*dev_op_cpl)(struct rte_vhost2_dev *vdev, int rc);
+	void (*dev_call)(struct rte_vhost2_dev *vdev, struct rte_vhost2_vq *vq);
+
+	TAILQ_ENTRY(vhost_transport_ops) tailq;
+};
+
+void vhost_transport_register(struct vhost_transport_ops *transport);
+
+#define VHOST_TRANSPORT_REGISTER(tr)					\
+	void _tr_init_##tr(void);					\
+	void __attribute__((constructor, used)) _tr_init_##tr(void)	\
+	{								\
+		vhost_transport_register(&tr);			\
+	}
+
+#endif
