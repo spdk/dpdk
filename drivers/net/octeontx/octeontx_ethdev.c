abff2c9f549bfff2896954546455e0b1e754724e
Pavan Nikhilesh <pbhagavatula@caviumnetworks.com>
2018-07-24 16:13:50 +0530

    net/octeontx: fix stop clearing Rx/Tx functions
    [ upstream commit efb9dd148e234aecd01966cd1d2aaa4b75836966 ]
    
    On dev_stop the Rx/Tx_burst functions are being set to NULL, this causes
    a SEGFAULT in cases where control path calls stop and a paket is still
    being processed by a worker.
    Instead clear the fastpath functions in dev_close.
    
    Fixes: da6c687471a3 ("net/octeontx: add start and stop support")
    
    Signed-off-by: Pavan Nikhilesh <pbhagavatula@caviumnetworks.com>
    Acked-by: Jerin Jacob <jerin.jacob@caviumnetworks.com>

--- drivers/net/octeontx/octeontx_ethdev.c
+++ drivers/net/octeontx/octeontx_ethdev.c
@@ -352,6 +352,9 @@ octeontx_dev_close(struct rte_eth_dev *dev)
 
 		rte_free(txq);
 	}
+
+	dev->tx_pkt_burst = NULL;
+	dev->rx_pkt_burst = NULL;
 }
 
 static int
@@ -445,9 +448,6 @@ octeontx_dev_stop(struct rte_eth_dev *dev)
 			     ret);
 		return;
 	}
-
-	dev->tx_pkt_burst = NULL;
-	dev->rx_pkt_burst = NULL;
 }
 
 static void
