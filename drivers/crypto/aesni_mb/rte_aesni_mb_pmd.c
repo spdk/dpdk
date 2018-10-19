ad3955d59721c8be77437434b29e1dc77c53ae93
Darek Stojaczyk <dariusz.stojaczyk@intel.com>
2018-10-19 14:32:05 +0200

    Revert "crypto/aesni_mb: call buffer manager allocation"
    This reverts commit 862c0a9643cf5d4d3590043ad978dd49a6ac9c6c.
    
    rte_cryptodev offers an API function to allocate a queue pair,
    but not to free it. rte_compressdev offers both of those
    functions, so it's probably an overlook in rte_cryptodev design.
    
    By not freeing the queues we leak some memory allocated by
    rte_*alloc, but that doesn't cause us much trouble as it's
    backed by hugetlbfs. The patch that we're reverting here
    allocates some OS memory and that's now detected by our ASAN.
    
    While we wait for additional API to free the queues, lets
    just revert the patch that makes our CI fail. There will be
    some global memory used rather than per-queue dynamically
    allocated mem, but it doesn't come with any functional changes.
    
    Change-Id: Ia481b04a5e3a3dac6d52d81e968535ebb81d1322
    Signed-off-by: Darek Stojaczyk <dariusz.stojaczyk@intel.com>
    Reviewed-on: https://review.gerrithub.io/430060 (spdk-18.08)
    Reviewed-on: https://review.gerrithub.io/c/440313
    Reviewed-by: Jim Harris <james.r.harris@intel.com>
    Reviewed-by: Ben Walker <benjamin.walker@intel.com>
    Tested-by: Jim Harris <james.r.harris@intel.com>

--- drivers/crypto/aesni_mb/rte_aesni_mb_pmd.c
+++ drivers/crypto/aesni_mb/rte_aesni_mb_pmd.c
@@ -1012,7 +1012,7 @@ handle_completed_jobs(struct aesni_mb_qp *qp, JOB_AES_HMAC *job,
 		if (processed_jobs == nb_ops)
 			break;
 
-		job = (*qp->op_fns->job.get_completed_job)(qp->mb_mgr);
+		job = (*qp->op_fns->job.get_completed_job)(&qp->mb_mgr);
 	}
 
 	return processed_jobs;
@@ -1025,7 +1025,7 @@ flush_mb_mgr(struct aesni_mb_qp *qp, struct rte_crypto_op **ops,
 	int processed_ops = 0;
 
 	/* Flush the remaining jobs */
-	JOB_AES_HMAC *job = (*qp->op_fns->job.flush_job)(qp->mb_mgr);
+	JOB_AES_HMAC *job = (*qp->op_fns->job.flush_job)(&qp->mb_mgr);
 
 	if (job)
 		processed_ops += handle_completed_jobs(qp, job,
@@ -1065,7 +1065,7 @@ aesni_mb_pmd_dequeue_burst(void *queue_pair, struct rte_crypto_op **ops,
 	uint8_t digest_idx = qp->digest_idx;
 	do {
 		/* Get next free mb job struct from mb manager */
-		job = (*qp->op_fns->job.get_next)(qp->mb_mgr);
+		job = (*qp->op_fns->job.get_next)(&qp->mb_mgr);
 		if (unlikely(job == NULL)) {
 			/* if no free mb job structs we need to flush mb_mgr */
 			processed_jobs += flush_mb_mgr(qp,
@@ -1075,7 +1075,7 @@ aesni_mb_pmd_dequeue_burst(void *queue_pair, struct rte_crypto_op **ops,
 			if (nb_ops == processed_jobs)
 				break;
 
-			job = (*qp->op_fns->job.get_next)(qp->mb_mgr);
+			job = (*qp->op_fns->job.get_next)(&qp->mb_mgr);
 		}
 
 		/*
@@ -1095,7 +1095,7 @@ aesni_mb_pmd_dequeue_burst(void *queue_pair, struct rte_crypto_op **ops,
 		}
 
 		/* Submit job to multi-buffer for processing */
-		job = (*qp->op_fns->job.submit)(qp->mb_mgr);
+		job = (*qp->op_fns->job.submit)(&qp->mb_mgr);
 
 		/*
 		 * If submit returns a processed job then handle it,
