87da7f30007e2e5560d96dd42c668312eb3375ca
Nikos Dragazis <ndragazis@arrikto.com>
2019-03-03 18:58:59 +0200

    vhost: move vhost_user.fdset to trans_af_unix.c
    The fdset is used by the AF_UNIX transport code but other transports may
    not need it.  Move it to trans_af_unix.c and then make struct vhost_user
    private again since nothing outside socket.c needs it.
    
    Signed-off-by: Nikos Dragazis <ndragazis@arrikto.com>
    Change-Id: I5589627508bdd32bcfec4ad30efaeab1b4fa45e1

--- lib/librte_vhost/trans_af_unix.c
+++ lib/librte_vhost/trans_af_unix.c
@@ -10,10 +10,18 @@
 
 #include <rte_log.h>
 
+#include "fd_man.h"
 #include "vhost.h"
 #include "vhost_user.h"
 
 #define MAX_VIRTIO_BACKLOG 128
+
+static struct fdset af_unix_fdset = {
+	.fd = { [0 ... MAX_FDS - 1] = {-1, NULL, NULL, NULL, 0} },
+	.fd_mutex = PTHREAD_MUTEX_INITIALIZER,
+	.fd_pooling_mutex = PTHREAD_MUTEX_INITIALIZER,
+	.num = 0
+};
 
 TAILQ_HEAD(vhost_user_connection_list, vhost_user_connection);
 
@@ -189,7 +197,7 @@ vhost_user_add_connection(int fd, struct vhost_user_socket *vsocket)
 	conn->connfd = fd;
 	conn->vsocket = vsocket;
 	conn->vid = vid;
-	ret = fdset_add(&vhost_user.fdset, fd, vhost_user_read_cb,
+	ret = fdset_add(&af_unix_fdset, fd, vhost_user_read_cb,
 			NULL, conn);
 	if (ret < 0) {
 		RTE_LOG(ERR, VHOST_CONFIG,
@@ -206,7 +214,7 @@ vhost_user_add_connection(int fd, struct vhost_user_socket *vsocket)
 	TAILQ_INSERT_TAIL(&af_vsocket->conn_list, conn, next);
 	pthread_mutex_unlock(&af_vsocket->conn_mutex);
 
-	fdset_pipe_notify(&vhost_user.fdset);
+	fdset_pipe_notify(&af_unix_fdset);
 	return;
 
 err:
@@ -322,7 +330,7 @@ vhost_user_start_server(struct vhost_user_socket *vsocket)
 	if (ret < 0)
 		goto err;
 
-	ret = fdset_add(&vhost_user.fdset, fd, vhost_user_server_new_connection,
+	ret = fdset_add(&af_unix_fdset, fd, vhost_user_server_new_connection,
 		  NULL, vsocket);
 	if (ret < 0) {
 		RTE_LOG(ERR, VHOST_CONFIG,
@@ -548,7 +556,7 @@ af_unix_socket_cleanup(struct vhost_user_socket *vsocket)
 	struct vhost_user_connection *conn, *next;
 
 	if (vsocket->is_server) {
-		fdset_del(&vhost_user.fdset, af_vsocket->socket_fd);
+		fdset_del(&af_unix_fdset, af_vsocket->socket_fd);
 		close(af_vsocket->socket_fd);
 		unlink(vsocket->path);
 	} else if (vsocket->reconnect) {
@@ -567,7 +575,7 @@ again:
 		 * conn_mutex lock, and try again since
 		 * the r/wcb may use the conn_mutex lock.
 		 */
-		if (fdset_try_del(&vhost_user.fdset,
+		if (fdset_try_del(&af_unix_fdset,
 				  conn->connfd) == -1) {
 			pthread_mutex_unlock(
 					&af_vsocket->conn_mutex);
@@ -590,6 +598,31 @@ again:
 static int
 af_unix_socket_start(struct vhost_user_socket *vsocket)
 {
+	static pthread_t fdset_tid;
+
+	if (fdset_tid == 0) {
+		/**
+		 * create a pipe which will be waited by poll and notified to
+		 * rebuild the wait list of poll.
+		 */
+		if (fdset_pipe_init(&af_unix_fdset) < 0) {
+			RTE_LOG(ERR, VHOST_CONFIG,
+				"failed to create pipe for vhost fdset\n");
+			return -1;
+		}
+
+		int ret = rte_ctrl_thread_create(&fdset_tid,
+			"vhost-events", NULL, fdset_event_dispatch,
+			&af_unix_fdset);
+		if (ret != 0) {
+			RTE_LOG(ERR, VHOST_CONFIG,
+				"failed to create fdset handling thread");
+
+			fdset_pipe_uninit(&af_unix_fdset);
+			return -1;
+		}
+	}
+
 	if (vsocket->is_server)
 		return vhost_user_start_server(vsocket);
 	else
