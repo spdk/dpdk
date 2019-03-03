cff500d0ce2903582d371f5a4e44367cbd0e18f8
Nikos Dragazis <ndragazis@arrikto.com>
2019-03-03 18:54:44 +0200

    vhost: move AF_UNIX code from socket.c to trans_af_unix.c
    The socket.c file serves two purposes:
    1. librte_vhost public API entry points, e.g. rte_vhost_driver_register().
    2. AF_UNIX socket management.
    
    Move AF_UNIX socket code into trans_af_unix.c so that socket.c only
    handles the librte_vhost public API entry points.  This will make it
    possible to support other transports besides AF_UNIX.
    
    This patch is a preparatory step that simply moves code from socket.c to
    trans_af_unix.c unmodified, besides dropping 'static' qualifiers where
    necessary because socket.c now calls into trans_af_unix.c.
    
    A lot of socket.c state is exposed in vhost.h but this is a temporary
    measure and will be cleaned up in later patches.  By simply moving code
    unmodified in this patch it will be easier to review the actual
    refactoring that follows.
    
    Signed-off-by: Nikos Dragazis <ndragazis@arrikto.com>
    Change-Id: I7135ca9a28f7b9b8d9ee9c0e1012e073fd4602e1

--- lib/librte_vhost/vhost.h
+++ lib/librte_vhost/vhost.h
@@ -5,6 +5,7 @@
 #ifndef _VHOST_NET_CDEV_H_
 #define _VHOST_NET_CDEV_H_
 #include <stdint.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <sys/types.h>
@@ -13,13 +14,16 @@
 #include <linux/vhost.h>
 #include <linux/virtio_net.h>
 #include <sys/socket.h>
+#include <sys/un.h> /* TODO remove when trans_af_unix.c refactoring is done */
 #include <linux/if.h>
+#include <pthread.h>
 
 #include <rte_log.h>
 #include <rte_ether.h>
 #include <rte_rwlock.h>
 #include <rte_malloc.h>
 
+#include "fd_man.h"
 #include "rte_vhost.h"
 #include "rte_vdpa.h"
 
@@ -360,19 +364,91 @@ struct virtio_net {
 	struct rte_vhost_user_extern_ops extern_ops;
 } __rte_cache_aligned;
 
+/* The vhost_user, vhost_user_socket, vhost_user_connection, and reconnect
+ * declarations are temporary measures for moving AF_UNIX code into
+ * trans_af_unix.c.  They will be cleaned up as socket.c is untangled from
+ * trans_af_unix.c.
+ */
+TAILQ_HEAD(vhost_user_connection_list, vhost_user_connection);
+
+/*
+ * Every time rte_vhost_driver_register() is invoked, an associated
+ * vhost_user_socket struct will be created.
+ */
+struct vhost_user_socket {
+	struct vhost_user_connection_list conn_list;
+	pthread_mutex_t conn_mutex;
+	char *path;
+	int socket_fd;
+	struct sockaddr_un un;
+	bool is_server;
+	bool reconnect;
+	bool dequeue_zero_copy;
+	bool iommu_support;
+	bool use_builtin_virtio_net;
+
+	/*
+	 * The "supported_features" indicates the feature bits the
+	 * vhost driver supports. The "features" indicates the feature
+	 * bits after the rte_vhost_driver_features_disable/enable().
+	 * It is also the final feature bits used for vhost-user
+	 * features negotiation.
+	 */
+	uint64_t supported_features;
+	uint64_t features;
+
+	uint64_t protocol_features;
+
+	/*
+	 * Device id to identify a specific backend device.
+	 * It's set to -1 for the default software implementation.
+	 * If valid, one socket can have 1 connection only.
+	 */
+	int vdpa_dev_id;
+
+	struct vhost_device_ops const *notify_ops;
+};
+
+struct vhost_user_connection {
+	struct vhost_user_socket *vsocket;
+	int connfd;
+	int vid;
+
+	TAILQ_ENTRY(vhost_user_connection) next;
+};
+
+#define MAX_VHOST_SOCKET 1024
+struct vhost_user {
+	struct vhost_user_socket *vsockets[MAX_VHOST_SOCKET];
+	struct fdset fdset;
+	int vsocket_cnt;
+	pthread_mutex_t mutex;
+};
+
+extern struct vhost_user vhost_user;
+
+int create_unix_socket(struct vhost_user_socket *vsocket);
+int vhost_user_start_server(struct vhost_user_socket *vsocket);
+int vhost_user_start_client(struct vhost_user_socket *vsocket);
+
+extern pthread_t reconn_tid;
+
+int vhost_user_reconnect_init(void);
+bool vhost_user_remove_reconnect(struct vhost_user_socket *vsocket);
+
 static __rte_always_inline bool
 vq_is_packed(struct virtio_net *dev)
 {
-	return dev->features & (1ull << VIRTIO_F_RING_PACKED);
+	        return dev->features & (1ull << VIRTIO_F_RING_PACKED);
 }
 
 static inline bool
 desc_is_avail(struct vring_packed_desc *desc, bool wrap_counter)
 {
-	uint16_t flags = *((volatile uint16_t *) &desc->flags);
+	        uint16_t flags = *((volatile uint16_t *) &desc->flags);
 
-	return wrap_counter == !!(flags & VRING_DESC_F_AVAIL) &&
-		wrap_counter != !!(flags & VRING_DESC_F_USED);
+		        return wrap_counter == !!(flags & VRING_DESC_F_AVAIL) &&
+				                wrap_counter != !!(flags & VRING_DESC_F_USED);
 }
 
 #define VHOST_LOG_PAGE	4096
