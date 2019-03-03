8ab3b8de70925eb7de6cdfd759b3e02e74157a03
Nikos Dragazis <ndragazis@arrikto.com>
2019-03-03 18:58:04 +0200

    vhost: move start_server/client() calls to trans_af_unix.c
    Introduce a vhost_transport_ops->socket_start() interface so the
    transport can begin establishing vhost-user connections.  This is part
    of the AF_UNIX transport refactoring and removes AF_UNIX code from
    vhost.h and socket.c.
    
    Signed-off-by: Nikos Dragazis <ndragazis@arrikto.com>
    Change-Id: Ic58f2d10d08bc74f5d752ba8aa1f85889df054ae

--- lib/librte_vhost/trans_af_unix.c
+++ lib/librte_vhost/trans_af_unix.c
@@ -51,6 +51,8 @@ struct af_unix_socket {
 };
 
 static int create_unix_socket(struct vhost_user_socket *vsocket);
+static int vhost_user_start_server(struct vhost_user_socket *vsocket);
+static int vhost_user_start_client(struct vhost_user_socket *vsocket);
 static void vhost_user_read_cb(int connfd, void *dat, int *remove);
 
 /*
@@ -301,7 +303,7 @@ create_unix_socket(struct vhost_user_socket *vsocket)
 	return 0;
 }
 
-int
+static int
 vhost_user_start_server(struct vhost_user_socket *vsocket)
 {
 	struct af_unix_socket *s =
@@ -459,7 +461,7 @@ vhost_user_reconnect_init(void)
 	return ret;
 }
 
-int
+static int
 vhost_user_start_client(struct vhost_user_socket *vsocket)
 {
 	struct af_unix_socket *s =
@@ -550,6 +552,15 @@ af_unix_socket_cleanup(struct vhost_user_socket *vsocket)
 }
 
 static int
+af_unix_socket_start(struct vhost_user_socket *vsocket)
+{
+	if (vsocket->is_server)
+		return vhost_user_start_server(vsocket);
+	else
+		return vhost_user_start_client(vsocket);
+}
+
+static int
 af_unix_vring_call(struct virtio_net *dev __rte_unused,
 		   struct vhost_virtqueue *vq)
 {
@@ -562,5 +573,6 @@ const struct vhost_transport_ops af_unix_trans_ops = {
 	.socket_size = sizeof(struct af_unix_socket),
 	.socket_init = af_unix_socket_init,
 	.socket_cleanup = af_unix_socket_cleanup,
+	.socket_start = af_unix_socket_start,
 	.vring_call = af_unix_vring_call,
 };
