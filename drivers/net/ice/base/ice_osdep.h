f50974db04c6890ea52c51395af5252f690bce71
Haiyue Wang <haiyue.wang@intel.com>
2020-03-31 14:50:34 +0800

    net/ice/base: check memory pointer before copying
    [ upstream commit 2adbccec40663c457a47391b8d704bceb579c2f0 ]
    
    The ice_memdup doesn't check the new allocated memory pointer, it calls
    the rte_memcpy directly. It should check it.
    
    Fixes: 5f0978e96220 ("net/ice/base: add OS specific implementation")
    
    Signed-off-by: Haiyue Wang <haiyue.wang@intel.com>
    Reviewed-by: Xiaolong Ye <xiaolong.ye@intel.com>

--- drivers/net/ice/base/ice_osdep.h
+++ drivers/net/ice/base/ice_osdep.h
@@ -24,6 +24,8 @@
 #include <rte_random.h>
 #include <rte_io.h>
 
+#include "ice_alloc.h"
+
 #include "../ice_logs.h"
 
 #define INLINE inline
@@ -176,7 +178,6 @@ struct ice_virt_mem {
 
 #define ice_memset(a, b, c, d) memset((a), (b), (c))
 #define ice_memcpy(a, b, c, d) rte_memcpy((a), (b), (c))
-#define ice_memdup(a, b, c, d) rte_memcpy(ice_malloc(a, c), b, c)
 
 #define CPU_TO_BE16(o) rte_cpu_to_be_16(o)
 #define CPU_TO_BE32(o) rte_cpu_to_be_32(o)
@@ -222,6 +223,19 @@ ice_destroy_lock(__attribute__((unused)) struct ice_lock *sp)
 }
 
 struct ice_hw;
+
+static __rte_always_inline void *
+ice_memdup(__rte_unused struct ice_hw *hw, const void *src, size_t size,
+	   __rte_unused enum ice_memcpy_type dir)
+{
+	void *p;
+
+	p = ice_malloc(hw, size);
+	if (p)
+		rte_memcpy(p, src, size);
+
+	return p;
+}
 
 static inline void *
 ice_alloc_dma_mem(__attribute__((unused)) struct ice_hw *hw,
