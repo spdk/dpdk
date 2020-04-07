461a2ba67a7e9836cd99aab3d0d585eed317c5bd
Bernard Iremonger <bernard.iremonger@intel.com>
2020-04-07 16:26:41 +0100

    net/i40e: fix flow director initialisation
    [ upstream commit ae08c73e6f55e62e402fcdbfa6e8e967bf4bc5cc ]
    
    Remove references to struct rte_fdir_conf which is deprecated,
    in i40e_flow_parse_fdir_filter(), i40e_flow_destroy() and
    i40e_flow_add_del_fdir_filter().
    
    Fixes: c3be43817cfb ("net/i40e: fix unexpected skip FDIR setup")
    Fixes: 2e67a7fbf3ff ("net/i40e: config flow director automatically")
    Fixes: 4149825bbdb9 ("net/i40e: finish integration FDIR with generic flow API")
    
    Signed-off-by: Bernard Iremonger <bernard.iremonger@intel.com>
    Acked-by: Beilei Xing <beilei.xing@intel.com>

--- drivers/net/i40e/i40e_flow.c
+++ drivers/net/i40e/i40e_flow.c
@@ -3208,8 +3208,7 @@ i40e_flow_parse_fdir_filter(struct rte_eth_dev *dev,
 
 	cons_filter_type = RTE_ETH_FILTER_FDIR;
 
-	if (dev->data->dev_conf.fdir_conf.mode != RTE_FDIR_MODE_PERFECT ||
-		pf->fdir.fdir_vsi == NULL) {
+	if (pf->fdir.fdir_vsi == NULL) {
 		/* Enable fdir when fdir flow is added at first time. */
 		ret = i40e_fdir_setup(pf);
 		if (ret != I40E_SUCCESS) {
@@ -3225,8 +3224,6 @@ i40e_flow_parse_fdir_filter(struct rte_eth_dev *dev,
 					   NULL, "Failed to configure fdir.");
 			goto err;
 		}
-
-		dev->data->dev_conf.fdir_conf.mode = RTE_FDIR_MODE_PERFECT;
 	}
 
 	return 0;
@@ -4797,8 +4794,6 @@ i40e_flow_destroy(struct rte_eth_dev *dev,
 		/* If the last flow is destroyed, disable fdir. */
 		if (!ret && TAILQ_EMPTY(&pf->fdir.fdir_list)) {
 			i40e_fdir_teardown(pf);
-			dev->data->dev_conf.fdir_conf.mode =
-				   RTE_FDIR_MODE_NONE;
 			i40e_fdir_rx_proc_enable(dev, 0);
 		}
 		break;
