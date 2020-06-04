b4c9242d0552f5a8bbd88adb45e7d1ae548d5d5a
Pablo de Lara <pablo.de.lara.guarch@intel.com>
2020-06-04 08:51:12 +0000

    drivers/crypto: add missing OOP feature flag
    [ upstream commit cd346367f898d619edf53f13628d6e539dbcab40 ]
    
    ZUC, SNOW3G and KASUMI PMDs support Out-of-place operations,
    but their feature flags did not reflect this.
    
    Fixes: 2717246ecd7d ("cryptodev: replace mbuf scatter gather flag")
    
    Signed-off-by: Pablo de Lara <pablo.de.lara.guarch@intel.com>

--- drivers/crypto/snow3g/rte_snow3g_pmd.c
+++ drivers/crypto/snow3g/rte_snow3g_pmd.c
@@ -568,6 +568,7 @@ cryptodev_snow3g_create(const char *name,
 
 	dev->feature_flags = RTE_CRYPTODEV_FF_SYMMETRIC_CRYPTO |
 			RTE_CRYPTODEV_FF_SYM_OPERATION_CHAINING |
+			RTE_CRYPTODEV_FF_OOP_LB_IN_LB_OUT |
 			cpu_flags;
 
 	internals = dev->data->dev_private;
