spdk/dpdk

commit b3e01cd3e46fde2a6872b5b058890ac403701504

common/mlx5: fix build with -fno-common
[ upstream commit 2889d38256906a5cbff717e7e524ec645c89c794 ]

The variable storages of the same name are merged together
if compiled with -fcommon. This is the default.
This default behaviour allows to declare a variable in a header file and
share the variable in every .o binaries thanks to merge at link-time.

In the case of dlopen linking of the glue library, the pointer mlx5_glue
is referencing the glue functions struct and is set after calling
dlopen.

If compiling with -fno-common (default in GCC 10), the variable must be
declared as extern to avoid multiple re-definitions.
In case the glue layer is split in glue library, the variable mlx5_glue
needs to have its own storage for the rest of the PMD.

Signed-off-by: Thomas Monjalon <thomas@monjalon.net>
Acked-by: Matan Azrad <matan@mellanox.com>

drivers/net/mlx5/mlx5_glue.c
@@ -30,6 +30,10 @@
 #include "mlx5_autoconf.h"
 #include "mlx5_glue.h"
 
+#ifdef MLX5_GLUE
+const struct mlx5_glue *mlx5_glue;
+#endif
+
 static int
 mlx5_glue_fork_init(void)
 {

drivers/net/mlx5/mlx5_glue.h
@@ -258,6 +258,6 @@ struct mlx5_glue {
 			       struct mlx5dv_devx_port *mlx5_devx_port);
 };
 
-const struct mlx5_glue *mlx5_glue;
+extern const struct mlx5_glue *mlx5_glue;
 
 #endif /* MLX5_GLUE_H_ */

commit 3196e264f7f7bef148620bd34ba542cd08c7d630

net/mlx5: fix build with separate glue lib for dlopen
If building with option RTE_IBVERBS_LINK_DLOPEN (make)
or ibverbs_link=dlopen (meson), the glue is a separate library
which can be dlopen.
In such case, the object mlx5_glue must be allocated in two places:
one in the glue, and one outside of the glue to be assigned when
loading the glue with dlopen/dlsym.

When backporting the allocation split for gcc 10, both allocations
were made inside the glue. As a consequence, the symbol is undefined
outside of the glue:
	drivers/net/mlx5/mlx5.c: undefined reference to 'mlx5_glue'

The second allocation is restored outside of the glue, in mlx5.c.

Fixes: b3e01cd3e46f ("common/mlx5: fix build with -fno-common")

Signed-off-by: Thomas Monjalon <thomas@monjalon.net>
Acked-by: Luca Boccassi <bluca@debian.org>

drivers/net/mlx5/mlx5.c
@@ -187,6 +187,10 @@ struct mlx5_dev_spawn_data {
 	struct rte_pci_device *pci_dev; /**< Backend PCI device. */
 };
 
+#ifdef MLX5_GLUE
+const struct mlx5_glue *mlx5_glue;
+#endif
+
 static LIST_HEAD(, mlx5_ibv_shared) mlx5_ibv_list = LIST_HEAD_INITIALIZER();
 static pthread_mutex_t mlx5_ibv_list_mutex = PTHREAD_MUTEX_INITIALIZER;
 

drivers/net/mlx5/mlx5_glue.c
@@ -30,10 +30,6 @@
 #include "mlx5_autoconf.h"
 #include "mlx5_glue.h"
 
-#ifdef MLX5_GLUE
-const struct mlx5_glue *mlx5_glue;
-#endif
-
 static int
 mlx5_glue_fork_init(void)
 {