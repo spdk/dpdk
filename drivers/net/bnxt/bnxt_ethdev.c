7609e583d734dd259a69fd140035d43194b98628
Lance Richardson <lance.richardson@broadcom.com>
2020-09-22 13:30:35 -0400

    net/bnxt: fix queue get info
    [ upstream commit bd6345e4f48a737ec498d013953e785ab185e5f1 ]
    
    Return current offloads in rxq_info_get()/txq_info_get().
    
    Fixes: 2fc201884be8 ("net/bnxt: support rxq/txq get information")
    
    Signed-off-by: Lance Richardson <lance.richardson@broadcom.com>
    Reviewed-by: Somnath Kotur <somnath.kotur@broadcom.com>
    Reviewed-by: Ajit Khaparde <ajit.khaparde@broadcom.com>

--- drivers/net/bnxt/bnxt_ethdev.c
+++ drivers/net/bnxt/bnxt_ethdev.c
@@ -2205,6 +2205,7 @@ bnxt_rxq_info_get_op(struct rte_eth_dev *dev, uint16_t queue_id,
 	qinfo->conf.rx_free_thresh = rxq->rx_free_thresh;
 	qinfo->conf.rx_drop_en = rxq->drop_en;
 	qinfo->conf.rx_deferred_start = rxq->rx_deferred_start;
+	qinfo->conf.offloads = dev->data->dev_conf.rxmode.offloads;
 }
 
 static void
@@ -2228,6 +2229,7 @@ bnxt_txq_info_get_op(struct rte_eth_dev *dev, uint16_t queue_id,
 	qinfo->conf.tx_free_thresh = txq->tx_free_thresh;
 	qinfo->conf.tx_rs_thresh = 0;
 	qinfo->conf.tx_deferred_start = txq->tx_deferred_start;
+	qinfo->conf.offloads = dev->data->dev_conf.txmode.offloads;
 }
 
 int bnxt_mtu_set_op(struct rte_eth_dev *eth_dev, uint16_t new_mtu)
