38350a12b1b04ff57413075718cad0b6b6de5d01
Chengchang Tang <tangchengchang@huawei.com>
2020-09-25 20:47:14 +0800

    app/testpmd: fix port id check in Tx VLAN command
    [ upstream commit b428e71775e43acc7b074c82b0d9e08f401dde6b ]
    
    To set Tx vlan offloads, it is required to stop port firstly. But before
    checking whether the port is stopped, the port id entered by the user
    is not checked for validity. When the port id is illegal, it would lead
    to a segmentation fault since it attempts to access a member of
    non-existent port.
    
    This patch adds verification of port id in tx vlan offloads and remove
    duplicated check.
    
    Fixes: 597f9fafe13b ("app/testpmd: convert to new Tx offloads API")
    
    Signed-off-by: Chengchang Tang <tangchengchang@huawei.com>
    Signed-off-by: Wei Hu (Xavier) <xavier.huwei@huawei.com>
    Reviewed-by: Ferruh Yigit <ferruh.yigit@intel.com>

--- app/test-pmd/cmdline.c
+++ app/test-pmd/cmdline.c
@@ -4168,6 +4168,9 @@ cmd_tx_vlan_set_parsed(void *parsed_result,
 {
 	struct cmd_tx_vlan_set_result *res = parsed_result;
 
+	if (port_id_is_invalid(res->port_id, ENABLED_WARN))
+		return;
+
 	if (!port_is_stopped(res->port_id)) {
 		printf("Please stop port %d first\n", res->port_id);
 		return;
@@ -4221,6 +4224,9 @@ cmd_tx_vlan_set_qinq_parsed(void *parsed_result,
 			    __attribute__((unused)) void *data)
 {
 	struct cmd_tx_vlan_set_qinq_result *res = parsed_result;
+
+	if (port_id_is_invalid(res->port_id, ENABLED_WARN))
+		return;
 
 	if (!port_is_stopped(res->port_id)) {
 		printf("Please stop port %d first\n", res->port_id);
@@ -4334,6 +4340,9 @@ cmd_tx_vlan_reset_parsed(void *parsed_result,
 			 __attribute__((unused)) void *data)
 {
 	struct cmd_tx_vlan_reset_result *res = parsed_result;
+
+	if (port_id_is_invalid(res->port_id, ENABLED_WARN))
+		return;
 
 	if (!port_is_stopped(res->port_id)) {
 		printf("Please stop port %d first\n", res->port_id);
--- app/test-pmd/config.c
+++ app/test-pmd/config.c
@@ -3235,8 +3235,6 @@ tx_vlan_set(portid_t port_id, uint16_t vlan_id)
 	struct rte_eth_dev_info dev_info;
 	int ret;
 
-	if (port_id_is_invalid(port_id, ENABLED_WARN))
-		return;
 	if (vlan_id_is_invalid(vlan_id))
 		return;
 
@@ -3267,8 +3265,6 @@ tx_qinq_set(portid_t port_id, uint16_t vlan_id, uint16_t vlan_id_outer)
 	struct rte_eth_dev_info dev_info;
 	int ret;
 
-	if (port_id_is_invalid(port_id, ENABLED_WARN))
-		return;
 	if (vlan_id_is_invalid(vlan_id))
 		return;
 	if (vlan_id_is_invalid(vlan_id_outer))
@@ -3294,8 +3290,6 @@ tx_qinq_set(portid_t port_id, uint16_t vlan_id, uint16_t vlan_id_outer)
 void
 tx_vlan_reset(portid_t port_id)
 {
-	if (port_id_is_invalid(port_id, ENABLED_WARN))
-		return;
 	ports[port_id].dev_conf.txmode.offloads &=
 				~(DEV_TX_OFFLOAD_VLAN_INSERT |
 				  DEV_TX_OFFLOAD_QINQ_INSERT);
