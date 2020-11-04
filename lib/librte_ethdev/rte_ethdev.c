8d629b3a5d113c7ddb63868286a82505582f266e
Yunjian Wang <wangyunjian@huawei.com>
2020-11-04 10:57:57 +0800

    ethdev: fix data type for port id
    [ upstream commit 4bb02a6f5b7f11aafde1be7bfd08b6e81997b916 ]
    
    The ethdev port id is 16 bits now. This patch fixes the data type
    of the variable for 'pid', which changing from uint32_t to uint16_t.
    
    RTE_MAX_ETHPORTS is the maximum number of ports, which customized by
    the user. To avoid 16-bit unsigned integer overflow, the valid value
    of RTE_MAX_ETHPORTS should be set from 0 to UINT16_MAX, and it is
    safer to cut one more port from space.
    
    So we use RTE_BUILD_BUG_ON() to ensure that RTE_MAX_ETHPORTS is less
    to UINT16_MAX.
    
    Fixes: 5b7ba31148a8 ("ethdev: add port ownership")
    
    Signed-off-by: Yunjian Wang <wangyunjian@huawei.com>
    Acked-by: Ajit Khaparde <ajit.khaparde@broadcom.com>
    Reviewed-by: Ferruh Yigit <ferruh.yigit@intel.com>

--- lib/librte_ethdev/rte_ethdev.c
+++ lib/librte_ethdev/rte_ethdev.c
@@ -408,7 +408,9 @@ is_allocated(const struct rte_eth_dev *ethdev)
 static struct rte_eth_dev *
 _rte_eth_dev_allocated(const char *name)
 {
-	unsigned i;
+	uint16_t i;
+
+	RTE_BUILD_BUG_ON(RTE_MAX_ETHPORTS >= UINT16_MAX);
 
 	for (i = 0; i < RTE_MAX_ETHPORTS; i++) {
 		if (rte_eth_devices[i].data != NULL &&
@@ -437,7 +439,7 @@ rte_eth_dev_allocated(const char *name)
 static uint16_t
 rte_eth_dev_find_free_port(void)
 {
-	unsigned i;
+	uint16_t i;
 
 	for (i = 0; i < RTE_MAX_ETHPORTS; i++) {
 		/* Using shared name field to find a free port. */
@@ -800,7 +802,7 @@ rte_eth_dev_get_name_by_port(uint16_t port_id, char *name)
 int
 rte_eth_dev_get_port_by_name(const char *name, uint16_t *port_id)
 {
-	uint32_t pid;
+	uint16_t pid;
 
 	if (name == NULL) {
 		RTE_ETHDEV_LOG(ERR, "Null pointer is specified\n");
@@ -3954,7 +3956,7 @@ rte_eth_mirror_rule_reset(uint16_t port_id, uint8_t rule_id)
 
 RTE_INIT(eth_dev_init_cb_lists)
 {
-	int i;
+	uint16_t i;
 
 	for (i = 0; i < RTE_MAX_ETHPORTS; i++)
 		TAILQ_INIT(&rte_eth_devices[i].link_intr_cbs);
@@ -3967,7 +3969,7 @@ rte_eth_dev_callback_register(uint16_t port_id,
 {
 	struct rte_eth_dev *dev;
 	struct rte_eth_dev_callback *user_cb;
-	uint32_t next_port; /* size is 32-bit to prevent loop wrap-around */
+	uint16_t next_port;
 	uint16_t last_port;
 
 	if (!cb_fn)
@@ -4030,7 +4032,7 @@ rte_eth_dev_callback_unregister(uint16_t port_id,
 	int ret;
 	struct rte_eth_dev *dev;
 	struct rte_eth_dev_callback *cb, *next;
-	uint32_t next_port; /* size is 32-bit to prevent loop wrap-around */
+	uint16_t next_port;
 	uint16_t last_port;
 
 	if (!cb_fn)
@@ -5097,7 +5099,7 @@ static struct rte_eth_dev_switch {
 int
 rte_eth_switch_domain_alloc(uint16_t *domain_id)
 {
-	unsigned int i;
+	uint16_t i;
 
 	*domain_id = RTE_ETH_DEV_SWITCH_DOMAIN_ID_INVALID;
 
