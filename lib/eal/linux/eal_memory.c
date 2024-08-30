a0a715b11ad59c1ec9f94303e945345d02f6b43a
Artur Paszkiewicz <artur.paszkiewicz@intel.com>
2024-08-30 14:51:30 +0200

    mem: fix ASan shadow shared mem mapping
    msl->shm_fd was in shared memory and a secondary process could change
    it, causing the primary process to map wrong files into the shadow
    region. Fix it by keeping the file descriptors in a private array in
    each process.
    
    Signed-off-by: Artur Paszkiewicz <artur.paszkiewicz@intel.com>
    Reviewed-on: https://review.spdk.io/c/spdk/dpdk/+/26633 (spdk-25.07)
    Reviewed-on: https://review.spdk.io/c/spdk/dpdk/+/27398 (spdk-25.11)
    
    (cherry picked from commit d33d3f74c96d674e50f312b481aa7b57a86a1153)
    Change-Id: Iae2a13b3f054bdf52b1ff1c3e24ea155972f8caf
    Signed-off-by: Tomasz Zawadzki <tomasz.zawadzki@nutanix.com>
    Reviewed-on: https://review.spdk.io/c/spdk/dpdk/+/28661
    Reviewed-by: Konrad Sztyber <ksztyber@nvidia.com>
    Tested-by: Tomasz Zawadzki <tomasz@tzawadzki.com>
    Reviewed-by: Mateusz Kozlowski <mateusz.kozlowski@solidigm.com>

--- lib/eal/linux/eal_memory.c
+++ lib/eal/linux/eal_memory.c
@@ -1945,6 +1945,10 @@ memseg_secondary_init(void)
 	return 0;
 }
 
+#ifdef RTE_MALLOC_ASAN
+static int msl_asan_shadow_fd[RTE_MAX_MEMSEG_LISTS];
+#endif
+
 int
 rte_eal_memseg_init(void)
 {
@@ -1954,6 +1958,9 @@ rte_eal_memseg_init(void)
 #ifndef RTE_EAL_NUMA_AWARE_HUGEPAGES
 	const struct internal_config *internal_conf =
 		eal_get_internal_configuration();
+#endif
+#ifdef RTE_MALLOC_ASAN
+	int msl_idx;
 #endif
 	if (getrlimit(RLIMIT_NOFILE, &lim) == 0) {
 		/* set limit to maximum */
@@ -1977,6 +1984,11 @@ rte_eal_memseg_init(void)
 		EAL_LOG(WARNING, "Please use --legacy-mem option, or recompile with NUMA support.");
 	}
 #endif
+#ifdef RTE_MALLOC_ASAN
+	for (msl_idx = 0; msl_idx < RTE_MAX_MEMSEG_LISTS; msl_idx++) {
+		msl_asan_shadow_fd[msl_idx] = -1;
+	}
+#endif
 
 	return rte_eal_process_type() == RTE_PROC_PRIMARY ?
 #ifndef RTE_ARCH_64
@@ -1993,6 +2005,7 @@ eal_memseg_list_map_asan_shadow(struct rte_memseg_list *msl)
 {
 	const struct internal_config *internal_conf =
 			eal_get_internal_configuration();
+	int msl_idx = msl - rte_eal_get_configuration()->mem_config->memsegs;
 	int shm_oflag;
 	char shm_path[PATH_MAX];
 	int shm_fd;
@@ -2032,7 +2045,7 @@ eal_memseg_list_map_asan_shadow(struct rte_memseg_list *msl)
 		}
 	}
 
-	msl->shm_fd = shm_fd;
+	msl_asan_shadow_fd[msl_idx] = shm_fd;
 
 	return 0;
 }
@@ -2042,12 +2055,14 @@ eal_memseg_list_unmap_asan_shadow(struct rte_memseg_list *msl)
 {
 	const struct internal_config *internal_conf =
 			eal_get_internal_configuration();
+	int msl_idx = msl - rte_eal_get_configuration()->mem_config->memsegs;
+	int *shm_fd = &msl_asan_shadow_fd[msl_idx];
 
-	if (msl->shm_fd == -1)
+	if (*shm_fd == -1)
 		return;
 
-	close(msl->shm_fd);
-	msl->shm_fd = -1;
+	close(*shm_fd);
+	*shm_fd = -1;
 
 	if (munmap(ASAN_MEM_TO_SHADOW(msl->base_va),
 		   msl->len >> ASAN_SHADOW_SCALE) != 0)
@@ -2061,5 +2076,11 @@ eal_memseg_list_unmap_asan_shadow(struct rte_memseg_list *msl)
 			 msl->memseg_arr.name);
 		shm_unlink(shm_path);
 	}
+}
+
+int
+eal_memseg_list_get_asan_shadow_fd(int msl_idx)
+{
+	return msl_asan_shadow_fd[msl_idx];
 }
 #endif
